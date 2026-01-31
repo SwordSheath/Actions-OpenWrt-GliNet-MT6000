6221a9fcb100f0f9cca3c4a04d5ccdaca742b319
RedFree <ltby126yx@126.com>
2026-01-31 09:10:36 +0800

    Modify OpenWrt feed source and add new feeds
    Updated OpenWrt feed sources and added new repositories.

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,4 +14,7 @@
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i 's/git;openwrt-23\.05/git/g' feeds.conf.default
+sed -i 's/git;openwrt-23\.05/git;openwrt-25\.12/g' feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
